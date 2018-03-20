a4bfd55c77ff972b00d985a4bf0a0e83b43b8ce3
trevor <trevor.hallett.5@gmail.com>
2018-03-20 10:34:37 -0600

    Second commit, ready to turn in.

--- action.js
+++ action.js
@@ -1,10 +1,8 @@
-var number = 0;
 
 function newList(){
-    var taskClass ='list' + number + 'task';
     var listTitle = $('#listName').val();
     $('.overList').append('<div class="list">' +
-        '<div class="listHead list' + number + '">' +
+        '<div class="listHead">' +
         '<div class="titleSpot">' + listTitle +
          '<input type="text" id="newItem" placeholder="New list item"/>' +
         '</div>' +
@@ -17,27 +15,31 @@ function newList(){
         '</div>'
 
     );
-
-    number++;
-    console.log('Button clicked ' + number);
 }
 function newItem(task) {
     var taskTitle = $(task).parent().parent().find('#newItem').val();
-    $(task).parent().parent().parent().append('<div class="listItem">' + taskTitle + '<span class="fas fa-check" onclick="done(this)"></span></div>');
+    $(task).parent().parent().parent().append('<div class="listItem" contenteditable="true">' + taskTitle +
+        '<div class="listIcons">' +
+        '<span class="fas fa-check" onclick="done(this)" contenteditable="false"></span>' +
+        '<span class="fas fa-trash" onclick="deleteTask(this)"></span></div>' +
+        '</div>'
+    );
 }
 
 function deleteItem(a){
     var granny = $(a).parent().parent().parent();
-    $(granny).fadeOut('slow', function(){
+    $(granny).fadeOut('medium', function(){
         $(granny).remove();
     });
+}
 
+function deleteTask(task){
+    $(task).parent().parent().fadeOut('medium', function(){
+       $(task).parent().parent().remove();
+    });
 }
 
 function done(element){
-   $(element).parent().toggleClass('complete');
+   $(element).parent().parent().toggleClass('complete');
     $(element).toggleClass('checkedCheck');
 }
-// $('.overList .list .').on('dblclick','li', function(){
-//     $(this).toggleClass('strike').fadeOut('slow');
-// });
